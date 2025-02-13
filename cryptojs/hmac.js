25a02f12635a71eae7bfb845cd5cc9b627739698
wehmann007 <41209820+wehmann007@users.noreply.github.com>
2025-02-13 17:06:26 +0100

    Update hmac.js to use msg.key and msg.algorithm

--- cryptojs/hmac.js
+++ cryptojs/hmac.js
@@ -9,17 +9,24 @@ module.exports = function (RED) {
 		node.key = config.key;
 
 		node.on('input', function (msg) {
+			
+			var algorithm = Object.is(msg.algorithm, undefined) ? node.algorithm : msg.algorithm;
+			var key = Object.is(msg.key, undefined) ? node.key : msg.key;
+			
 			// check configurations
-			if(!node.algorithm || !node.key) {
+			if(!algorithm || !key) {
 				// rising misconfiguration error
-				node.error("Missing configuration, please check your algorithm or key.", msg);
+				//node.error("Missing configuration, please check your algorithm or key.", msg);
+				
+				node.error("Missing configuration, please check your algorithm, secret key, mode or padding.", msg);
+				
 			} else {
 				// check the payload
 				if(msg.payload) {
 					// debugging message
-					node.debug('Creating a digest of payload using '+node.algorithm);
+					node.debug('Creating a digest of payload using '+ algorithm);
 					// digest with CryptoJS
-					msg.payload = CryptoJS[node.algorithm](msg.payload, node.key).toString();
+					msg.payload = CryptoJS[algorithm](msg.payload, key).toString();
 				} else {
 					// debugging message
 					node.trace('Nothing to digest: empty payload');
